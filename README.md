9e86b798df0c8600bd7cf27fb8fb39f573de21b4
Jonathan Potts <jonathanpotts@outlook.com>
2021-03-17 14:45:26 -0700

    Fix spigot issues

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Cartography Table
+# CartographyTable
 
 A Minecraft server plugin for creating immersive 3D maps viewable on the web.
--- src/main/java/com/jonathanpotts/cartographytable/CartographyTable.java
+++ src/main/java/com/jonathanpotts/cartographytable/CartographyTable.java
@@ -1,6 +1,16 @@
 package com.jonathanpotts.cartographytable;
 
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.List;
+
 public class CartographyTable extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        List<World> worlds = Bukkit.getWorlds();
+
+        getLogger().info("First world is " + worlds.get(0).getName());
+    }
 }
