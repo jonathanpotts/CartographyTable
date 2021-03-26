804e53806ee12b7b99d0302374067ce92c389b48
Jonathan Potts <jonathanpotts@outlook.com>
2021-03-26 13:36:18 -0700

    Add webpack-dev-server

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+    mode: 'production',
     entry: './src/index.ts',
     devtool: 'inline-source-map',
     module: {
