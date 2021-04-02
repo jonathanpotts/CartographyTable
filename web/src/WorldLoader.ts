85925cde80b7400f2154f5ca3c85cabf217352ec
Jonathan Potts <jonathanpotts@outlook.com>
2021-04-01 20:54:55 -0700

    Start implementing structure

--- web/src/WorldLoader.ts
+++ web/src/WorldLoader.ts
@@ -1,9 +1,17 @@
 import { Engine } from '@babylonjs/core/Engines/engine';
+import { Color3 } from '@babylonjs/core/Maths/math.color';
+import { Scene } from '@babylonjs/core/scene';
 
 export default class WorldLoader {
   private engine: Engine;
 
-  constructor(engine: Engine) {
+  public constructor(engine: Engine) {
     this.engine = engine;
+  }
+
+  public loadWorld(name: string): Scene {
+    const scene = new Scene(this.engine);
+    scene.clearColor = Color3.Black().toColor4();
+    return scene;
   }
 }
--- web/src/index.ts
+++ web/src/index.ts
@@ -1,10 +1,4 @@
 import { Engine } from '@babylonjs/core/Engines/engine';
-import { Scene } from '@babylonjs/core/scene';
-import { UniversalCamera } from '@babylonjs/core/Cameras/universalCamera';
-import { Vector3 } from '@babylonjs/core/Maths/math.vector';
-import { MeshBuilder } from '@babylonjs/core/Meshes/meshBuilder';
-import { BackgroundMaterial } from '@babylonjs/core/Materials/Background/backgroundMaterial';
-import { Texture } from '@babylonjs/core/Materials/Textures/texture';
 
 import WorldLoader from './WorldLoader';
 
@@ -15,23 +9,7 @@ const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
 const engine = new Engine(canvas, true);
 
 const worldLoader = new WorldLoader(engine);
-
-function createScene(): Scene {
-  const scene = new Scene(engine);
-
-  const box = MeshBuilder.CreateBox('box', {}, scene);
-
-  const dirtMat = new BackgroundMaterial('dirtMat', scene);
-  dirtMat.diffuseTexture = new Texture('data/textures/block/dirt.png', scene, true, false, Texture.NEAREST_NEAREST);
-  box.material = dirtMat;
-
-  const camera = new UniversalCamera('camera', new Vector3(0, 0, -10), scene);
-  camera.attachControl(canvas, true);
-
-  return scene;
-}
-
-const scene = createScene();
+const scene = worldLoader.loadWorld('world');
 
 engine.runRenderLoop(() => {
   scene.render();
